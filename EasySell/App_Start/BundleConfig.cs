71fdc08915192a6be20a6bbe5cda33252e3ae62d
Lin Shen <sl_nj_2004@hotmail.com>
2017-11-27 11:33:41 +1100

    fix font issue

--- EasySell/App_Start/BundleConfig.cs
+++ EasySell/App_Start/BundleConfig.cs
@@ -8,17 +8,21 @@ namespace EasySell
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/vendors/js").IncludeDirectory(
+            bundles.Add(new ScriptBundle("~/bundles/vendorsjs").IncludeDirectory(
             "~/Static/vendors/js", "*.js"));
+            bundles.Add(new StyleBundle("~/bundles/vendorscss").IncludeDirectory(
+            "~/Static/vendors/css", "*.css"));
+            
 
-            bundles.Add(new ScriptBundle("~/bundles/vendors/customjs").IncludeDirectory(
+            bundles.Add(new ScriptBundle("~/bundles/customjs").IncludeDirectory(
             "~/Static/vendors", "*.js"));
+            bundles.Add(new ScriptBundle("~/bundles/customcss").IncludeDirectory(
+            "~/Static/vendors", "*.css"));
 
-            bundles.Add(new StyleBundle("~/bundles/vendors/css").IncludeDirectory(
-                        "~/Static/vendors/css", "*.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/js").IncludeDirectory(
+            bundles.Add(new ScriptBundle("~/bundles/easyselljs").IncludeDirectory(
                         "~/Static/js", "*.js"));
+            BundleTable.EnableOptimizations = false;
+
         }
     }
 }
